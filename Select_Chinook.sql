14924405d884641b2d3136eb156db8efb6b5234c
KFranciszek <krzysztof.zernicki@gmail.com>
2017-11-02 17:07:07 +0100

    Update Select_Chinook.sql

--- Select_Chinook.sql
+++ Select_Chinook.sql
@@ -16,3 +16,26 @@ where UNITPRICE  BETWEEN 1 and 2 AND COMPOSER is null
 select name, round(milliseconds/60000,2)
 from track
 order by milliseconds DESC
+
+
+select  ar.name, ar.ARTISTID, al.title
+from ARTIST ar inner join album al
+on ar.ARTISTID  =al.ARTISTID
+
+select al.title as ALBUM, al.albumid, t.name as Piosenka
+from album al inner join track t 
+on al.albumid = t.albumid
+
+
+select al.title as ALBUM, count(t.name) as UtworyLiczba
+from album al inner join track t 
+on al.albumid = t.albumid
+group by al.TITLE
+ORDER BY al.TITLE
+
+
+select al.title as ALBUM, count(t.name) as UtworyLiczba, sum(round(t.MILLISECONDS/60000,2)) as TrwaMinut
+from album al inner join track t 
+on al.albumid = t.albumid
+group by al.TITLE
+
